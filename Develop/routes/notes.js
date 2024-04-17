c716ea3b6ba671c8f18c1504f74c5b65643b47be
Yan Ashe <yanashe@Yans-Mac-Studio.local>
2024-04-16 21:33:18 -0500

    added routes and utils

---
+++ Develop/routes/notes.js
@@ -0,0 +1,33 @@
+const notes = require('express').Router();
+
+const {readFromFile, readAndAppend} = require('../helpers/fsUtils')
+
+const notesData = require("../db/db.json")
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
+
+notes.post('/', (req, res) => {
+    console.info('request to save note')
+    console.log(req.body);
+    newNote = req.body
+    newNote.id = Math.floor((1 + Math.random())*0x100000).toString(16).substring(1);
+    console.log(newNote);
+    readAndAppend(newNote, './db/db.json')
+
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+});
+
+notes.delete('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => {
+        const oldData = data.find()
+    })
+})
+
+module.exports = notes
